8f6e3f980fb97fd18371c755783525f11ea51a52
JakubWójcik-dev <jakub-mail1@o2.pl>
2021-09-05 16:32:34 +0200

    init

--- src/Components/TimerWindow/index.js
+++ src/Components/TimerWindow/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { HideDiv, MainDiv, ReleaseOnClick,Button1,Button2,DivFlex } from './timerWindow.styles'
-
+import ArrowDown from '../../double-arrow-bottom.svg'
+import ArrowUp from '../../double-arrow-top.svg'
 const Window = ({item}) =>{
 
     return(
@@ -8,11 +9,15 @@ const Window = ({item}) =>{
        <ReleaseOnClick onClick={item}/> 
     <MainDiv>
        <DivFlex>
-           <button>
-               img
-               
-           </button>
-        <input type='number' placeholder='hours'name='hours'min='0' max='24'inputMode='none'/>
+           
+            <div width='5' height='5' display='inline-flex'>
+            <img src={ArrowUp} onClick={() => console.log("aut")} height='25' width='25' />
+            {/*<input type='number' placeholder='hours'name='hours'min='0' max='24'inputMode='none'/>*/}
+            <img src={ArrowDown} onClick={() => console.log("aut")} height='30' width='30' />
+            </div>
+  
+           
+        
         <input type='number' placeholder='min'  name='min'  min='0' max='60'inputMode='numeric'/>
         <input type='number' placeholder='sec'  name='sec'  min='0' max='60'inputMode='numeric'/>
         </DivFlex>
